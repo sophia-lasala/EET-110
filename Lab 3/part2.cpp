e0528f8dbed8c81b54d1ad31f1b3ff0d1ccb70ef
Sophia LaSala <sophia.lasala@outlook.com>
2026-09-15 17:08:20 -0400

    Update part2.cpp

--- Lab 3/part2.cpp
+++ Lab 3/part2.cpp
@@ -2,12 +2,24 @@
 using namespace std;
 #include <cmath> 
 
-double real, imaginary; 
+const double PI = 3.141592653589793238463;
+
+double real, imaginary, magnitude, angle; 
 
 int main(){
     cout << "Input real value (a): ";
     cin >> real;
 
-    cout << "Input imaginary value (b): ";
-    cout >> imaginary;    
+    cout << "\nInput imaginary value (b): ";
+    cin >> imaginary;    
+
+    cout << "\nYour number is: " << real << " + j" << imaginary;
+
+    magnitude = sqrt(pow(real, 2) + pow(imaginary, 2));
+    angle = atan(imaginary/real) * (180/PI) ;
+
+    cout << "\nYour magnitude is: " << magnitude;
+    cout << "\nYour angle is: " << angle;
+
+    return 0;
 }
