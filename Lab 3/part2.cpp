e7ac5a1730b23bb18df3b39924be4d3c2352c85e
Sophia LaSala <sophia.lasala@outlook.com>
2026-09-15 11:21:07 -0400

    Create part2.cpp

---
+++ Lab 3/part2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include <cmath> 
+
+double real, imaginary; 
+
+int main(){
+    cout << "Input real value (a): ";
+    cin >> real;
+
+    cout << "Input imaginary value (b): ";
+    cout >> imaginary;    
+}
