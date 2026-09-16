198610e4f0587c1e831a77c26ea40bb514f2b836
Sophia LaSala <sophia.lasala@outlook.com>
2026-09-16 12:32:38 -0400

    Create part1.cpp

---
+++ Lab 4/part1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+string input; 
+
+
+int main() 
+{   
+    do{
+        cout << ""
+    }
+    while ((input != "A") || (input != "B") || (input != "C")){
+
+    }
+    cout << "Hello, World!";
+    return 0;
+}
